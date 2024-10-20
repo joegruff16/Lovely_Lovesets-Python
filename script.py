8effced617af31dac1d15c41a0bc8ba51bfdc1e8
Joseph Ruff <joegruff16@gmail.com>
2024-10-20 17:25:09 -0400

    Added script file for our content for the inventory catalog. Added first item with price.

---
+++ script.py
@@ -0,0 +1,5 @@
+#First inventory item
+lovely_loveseat_description = "Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white."
+
+#Loveseat Price
+lovely_loveseat_price = 254.00
